436a5291200aea94b9f5632d880e3af0e62e7996
backgom <human264@gmail.com>
2025-09-24 20:31:41 +0900

    e

--- basic_syntax/1.basic_command.sh
+++ basic_syntax/1.basic_command.sh
@@ -4,3 +4,6 @@ git add .
 git commit -m "메세지"
 
 git push origin main
+git
+
+git commit
---
+++ basic_syntax/2.basic_command.sh
@@ -0,0 +1,17 @@
+#git hub 인증방식2가지
+#1. oauth인증방식
+#2. pat token
+
+
+
+
+
+
+git init
+
+git remote add origin 리포주소
+
+git remote remove origin
+
+git remote set-url origin 리포주소
+
